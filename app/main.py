ad3544e7d00b943f81306f11ed408b09936b8d47
shobhitm23 <shobhitm05@gmail.com>
2024-07-20 14:27:24 -0700

    changed to txt

--- app/main.py
+++ app/main.py
@@ -1,62 +1,57 @@
-from fastapi import FastAPI, Security, HTTPException
 import os
 from dotenv import load_dotenv
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.schema import Document
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from langchain_community.embeddings import OctoAIEmbeddings
 from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from pprint import pprint
-
 
 load_dotenv()
 
-# OCTOAI_API_TOKEN = os.getenv("OCTO_AI_TOKEN")
-from langchain_text_splitters import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter
+file_path = "CustomerData.txt"
 
-url = "https://en.wikipedia.org/wiki/Star_Wars"
+# Read the content of the file
+with open(file_path, 'r') as f:
+    file_text = f.read()
 
-headers_to_split_on = [
-    ("h1", "Header 1"),
-    ("h2", "Header 2"),
-    ("h3", "Header 3"),
-    ("h4", "Header 4"),
-    ("div", "Divider")
-]
-
-html_splitter = HTMLHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-
-# for local file use html_splitter.split_text_from_file(<path_to_file>)
-html_header_splits = html_splitter.split_text_from_url(url)
-
-chunk_size = 1024
-chunk_overlap = 128
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=chunk_size,
-    chunk_overlap=chunk_overlap,
+# Initialize the text splitter
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=512, chunk_overlap=64
 )
 
-# Split
-splits = text_splitter.split_documents(html_header_splits)
+# Split the text into chunks
+texts = text_splitter.split_text(file_text)
+
+# Create a list of Document objects for the split texts
+file_texts = [
+    Document(page_content=chunked_text, 
+             metadata={"doc_title": os.path.basename(file_path).split(".")[0], 
+                       "chunk_num": i})
+    for i, chunked_text in enumerate(texts)
+]
+
+embeddings = HuggingFaceEmbeddings()
+
+vector_store = FAISS.from_documents(
+    file_texts,
+    embedding=embeddings
+)
 
 llm = OctoAIEndpoint(
-        model="llama-2-13b-chat-fp16",
+        model="meta-llama-3-8b-instruct",
         max_tokens=1024,
         presence_penalty=0,
         temperature=0.1,
-        top_p=0.9
-    )
-embeddings = OctoAIEmbeddings(endpoint_url="https://text.octoai.run/v1/")
-
-vector_store = FAISS.from_documents(
-    splits,
-    embedding=embeddings
+        top_p=0.9,
 )
 
 retriever = vector_store.as_retriever()
 
-template="""You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+template="""You are an assistant for helping us make sales for our tires. Use the following pieces of retrieved context to give us the best response to our customers to ensure we can sell them new tires.
 Question: {question} 
 Context: {context} 
 Answer:"""
@@ -69,19 +64,19 @@ chain = (
     | StrOutputParser()
 )
 
-chain.invoke("Who is Luke's Father?")
+chain.invoke("I just got new tires. Why do I need new tires?")
 
-template="""You are a literary critic. You are given some context and asked to answer questions based on only that context.
-Question: {question} 
-Context: {context} 
-Answer:"""
-lit_crit_prompt = ChatPromptTemplate.from_template(template)
+# template="""You are a literary critic. You are given some context and asked to answer questions based on only that context.
+# Question: {question} 
+# Context: {context} 
+# Answer:"""
+# lit_crit_prompt = ChatPromptTemplate.from_template(template)
 
-lcchain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | lit_crit_prompt
-    | llm
-    | StrOutputParser()
-)
+# lcchain = (
+#     {"context": retriever, "question": RunnablePassthrough()}
+#     | lit_crit_prompt
+#     | llm
+#     | StrOutputParser()
+# )
 
-pprint(lcchain.invoke("What is the best thing about Luke's Father's story line?"))
+# pprint(lcchain.invoke("What is the best thing about Luke's Father's story line?"))
